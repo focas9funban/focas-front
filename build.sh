9945b827b63054c62e1caec50b2f82c9736e59b4
focas9funban <focas9funban@gmail.com>
2024-04-26 19:52:26 +0800

    設定調整

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-IP=$(ip addr |grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|grep -v 172| cut -d'/' -f 1)
+#IP=$(ip addr |grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|grep -v 172| cut -d'/' -f 1)
 FOCAS_ROOT=/usr/AP
 FOCAS_PATH=${FOCAS_ROOT}/focas
 FOCAS_LOG_PATH=${FOCAS_PATH}/logs
-ENV=prod
+#ENV=prod
 
 PID=$(ps -ef | grep java | grep api | awk '{print $2}')
 
-[ $IP = "10.0.2.29" ] && ENV="dev"
+#[ $IP = "10.0.2.29" ] && ENV="dev"
 
-echo this environment is [${ENV}]
+#echo this environment is [${ENV}]
 
 echo rebooting please wait...
 
@@ -28,7 +28,7 @@ sudo touch ${FOCAS_LOG_PATH}/info.log
 cd ${FOCAS_PATH}/target
 
 sudo nohup java \
-	 -jar api.jar --spring.profiles.active=${ENV} \
+	 -jar api.jar --spring.profiles.active=dev \
 	 > /dev/null 2>&1 &
 	 
-#tail -f ${FOCAS_LOG_PATH}/info.log
+tail -f ${FOCAS_LOG_PATH}/info.log
